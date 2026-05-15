2ea3e75bd7a04df6ebe3e09f986f67e4df7a2bfb
OmarSorin <omar.sorin22@gmail.com>
2026-05-15 22:27:01 +0300

    fixing checks 2.0

--- include/Enemy.h
+++ include/Enemy.h
@@ -63,7 +63,7 @@ public:
     // Returns true if the enemy died
     bool takeDamage(int amount);
 
-    static int getTotalEnemiesCreated() { return totalEnemiesCreated; }
+    // static int getTotalEnemiesCreated() { return totalEnemiesCreated; }
 
     friend std::ostream& operator<<(std::ostream& os, const Enemy& e);
 };
