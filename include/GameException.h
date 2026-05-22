155f73efaa71d200311b89c91d037c9695c56c8c
OmarSorin <omar.sorin22@gmail.com>
2026-05-22 13:48:43 +0300

    finally implemented exceptions, CLION is being an absolute headache :c , added exceptions to wrong indexing for weapons, playerposition and wrapped initialize function in a try and catch for a failsafe in case code doesn't run, should theoretically never trigger but you never know

---
+++ include/GameException.h
@@ -0,0 +1,34 @@
+#ifndef GAMEEXCEPTION_H
+#define GAMEEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class GameException : public std::exception {
+protected:
+    std::string message;
+public:
+    explicit GameException(const std::string& msg) : message(msg) {}
+    const char* what() const noexcept override { return message.c_str(); }
+};
+
+class InvalidPositionException : public GameException {
+public:
+    InvalidPositionException(int x, int y)
+        : GameException("Invalid position: (" + std::to_string(x) +
+                        ", " + std::to_string(y) + ")") {}
+};
+
+class DeadCharacterException : public GameException {
+public:
+    explicit DeadCharacterException(const std::string& name)
+        : GameException(name + " is dead and cannot act") {}
+};
+
+class InvalidItemException : public GameException {
+public:
+    explicit InvalidItemException(int index)
+        : GameException("Invalid inventory index: " + std::to_string(index)) {}
+};
+
+#endif // GAMEEXCEPTION_H
